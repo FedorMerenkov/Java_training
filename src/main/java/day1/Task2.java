e593334caf8d0b749ef8896d43d573627dd59605
FedorMerenkov <worend324@gmail.com>
2026-03-16 00:32:47 +0400

    add

--- src/main/java/day1/Task2.java
+++ src/main/java/day1/Task2.java
@@ -2,6 +2,11 @@ package day1;
 
 public class Task2 {
     public static void main(String[] args) {
+        String c2 = "";
+        for (int i = 0; i < 10; i++){
+            c2 += "JAVA ";
+        }
+        System.out.println(c2);
 
     }
 }
