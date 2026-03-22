927b91e30df6b5c6adf1b9acd6133b1887ad002b
FedorMerenkov <worend324@gmail.com>
2026-03-22 14:53:44 +0400

    clean master

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,24 +2,6 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 0;
-        int b1 = 0;
-        String c = " ";
-        String c2 = " ";
-        String c3 = "JAVA";
-        while (a>b){
-            c += "Java ";
-            b += 1;
-        }
-        //System.out.println(c);
-        for (int i = 0; i < 10; i++){
-             c2 += "Java ";
-        }
-        System.out.println(c2);
-        while (a>b1){
-            System.out.println(c3);
-            b1 += 1;
-        }
+
     }
 }
--- src/main/java/day1/Task5.java
+++ src/main/java/day1/Task5.java
@@ -2,10 +2,6 @@ package day1;
 
 public class Task5 {
     public static void main(String[] args) {
-        int year = 2020;
-        for (int i = 1980; i<=year; i += 4){
-            System.out.println("Олимпиада " + i + " года");
-        }
 
     }
 }
