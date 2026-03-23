aa947d4b31094d6b10c7166d287593a4d08c356c
FedorMerenkov <worend324@gmail.com>
2026-03-23 17:23:31 +0400

    Fix 3 day

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Task3 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             double a = s.nextDouble();
             double b = s.nextDouble();
             if (b == 0) {
