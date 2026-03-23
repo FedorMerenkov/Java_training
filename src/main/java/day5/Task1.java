b89612392f1a9074adeadef9ef421bb374c23430
FedorMerenkov <worend324@gmail.com>
2026-03-23 19:48:57 +0400

    complete day 5 task 2 fix task 1

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Task1 {
     public static void main(String[] args) {
-        Car Car1= new Car();
+        Car Car1 = new Car();
         Car1.setColor("Red");
         Car1.setYear(2000);
         Car1.setModel("BMW");
@@ -13,25 +13,32 @@ public class Task1 {
         System.out.println("Модель: " + Car1.getModel());
     }
 }
-class Car{
+
+class Car {
     int year;
     String color;
     String model;
+
     public void setYear(int yearCar) {
         year = yearCar;
     }
+
     public int getYear() {
         return year;
     }
+
     public void setColor(String colorCar) {
         color = colorCar;
     }
+
     public String getColor() {
         return color;
     }
+
     public void setModel(String modelCar) {
         model = modelCar;
     }
+
     public String getModel() {
         return model;
     }
