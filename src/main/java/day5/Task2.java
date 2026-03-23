c76c78cfc4e73028bce5c0c7b374d5a49463574a
FedorMerenkov <worend324@gmail.com>
2026-03-23 20:47:07 +0400

    Day 5  (#5)
    * complete day 5 task 1
    
    * complete day 5 task 2 fix task 1

--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,7 +1,37 @@
 package day5;
 
+import java.util.Arrays;
+
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike = new Motorbike("Red", "BMW", 2000);
+        System.out.println("Цвет: " + motorbike.getColor());
+        System.out.println("Год выпуска: " + motorbike.getYear());
+        System.out.println("Модель: " + motorbike.getModel());
+    }
+}
 
+class Motorbike {
+    int year;
+    String color;
+    String model;
+
+    public Motorbike(String color, String model, int year) {
+        this.color = color;
+        this.year = year;
+        this.model = model;
+        System.out.println("color = " + color + ", model = " + model + ", year = " + year);
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
     }
 }
