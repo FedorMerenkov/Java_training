56868cfba5338269b3142a936511c5e6518db943
FedorMerenkov <worend324@gmail.com>
2026-03-23 20:01:48 +0400

    Day 4 (#4)
    * complete day 4
    
    * Fix 4 day

--- src/main/java/day4/Task3.java
+++ src/main/java/day4/Task3.java
@@ -1,7 +1,30 @@
 package day4;
 
+import java.util.Arrays;
+
 public class Task3 {
     public static void main(String[] args) {
+        int n = 8;
+        int m = 12;
+        int max = 0;
+        int index = 0;
+        int[][] mass = new int[n][m];
+        for (int i = 0; i < mass.length; i++) {
+            int summa = 0;
+            for (int j = 0; j < mass[i].length; j++) {
+                mass[i][j] = (int) (Math.random() * 51);
+                summa = summa + mass[i][j];
+                System.out.print(mass[i][j] + " ");
+            }
+
+            if (max < summa) {
+                max = summa;
+                index = i;
+            }
+            System.out.print("\n");
+        }
+        System.out.print(" \n" + max + " \n");
+        System.out.print(index + " \n");
 
     }
 }
--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -1,7 +1,28 @@
 package day4;
 
+import java.util.Arrays;
+
 public class Task4 {
     public static void main(String[] args) {
-
+        int m = 100;
+        int summ ;
+        int[] mass = new int[m];
+        int max = 0;
+        int index = 0;
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = (int) (Math.random() * 10000);
+            System.out.println(mass[i]);
+        }
+        for (int i = 0; i < mass.length; i++) {
+            for (int j = 0; i >= j + 2; j++) {
+                summ = mass[i - 2] + mass[i - 1] + mass[i];
+                if (max < summ) {
+                    max = summ;
+                    index = i - 2;
+                }
+            }
+        }
+        System.out.println("max = " + max);
+        System.out.println("index = " + index);
     }
 }
