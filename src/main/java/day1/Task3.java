42de6c43b6ad38fef74bf2d70da68ce21e8d5aad
FedorMerenkov <worend324@gmail.com>
2026-03-23 20:57:18 +0400

    Day 1 (#1)
    * add
    
    * example 1 day
    
    * fix Task3, Task6
    
    * fix task1, task2, task4, task5

--- src/main/java/day1/Task3.java
+++ src/main/java/day1/Task3.java
@@ -2,6 +2,13 @@ package day1;
 
 public class Task3 {
     public static void main(String[] args) {
+        int a = 10;
+        int b1 = 0;
+        String c3 = "JAVA";
+        while (a > b1) {
+            System.out.println(c3);
+            b1 += 1;
+        }
 
     }
 }
