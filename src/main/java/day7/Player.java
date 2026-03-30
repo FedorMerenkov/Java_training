441ae81ca39c2e501496b7b950bb838c9bf80e47
FedorMerenkov <worend324@gmail.com>
2026-03-30 14:21:09 +0400

    complete day 7

---
+++ src/main/java/day7/Player.java
@@ -0,0 +1,50 @@
+package day7;
+
+import java.util.Random;
+
+public class Player {
+    int stamina;
+    private static final int MAX_STAMINA = 100;
+    private static final int MIN_STAMINA = 0;
+    private static int countPlayers = 0;
+
+    public Player(int stamina) {
+        if (countPlayers < 6) {
+            this.stamina = stamina;
+            countPlayers++;
+        } else {
+            return;
+        }
+    }
+
+    public static int getCountPlayers() {
+        return countPlayers;
+    }
+
+
+    public void run() {
+        if (stamina == MIN_STAMINA) {
+            return;
+        }
+        stamina -= 1;
+        if (stamina == MIN_STAMINA) {
+            System.out.println("Игроку " + countPlayers + " нужен отдых");
+            countPlayers--;
+        }
+    }
+
+
+
+    public static void info() {
+        if (countPlayers < 6 ) {
+            System.out.println("Команды неполные. На поле еще есть " + (6 - countPlayers) + " свободных мест");
+        } else if (countPlayers == 6) {
+            System.out.println("На поле нет свободных мест");
+        } else {
+            return;
+        }
+    }
+    public int getStamina() {
+        return stamina;
+    }
+}
