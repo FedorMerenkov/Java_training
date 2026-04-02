5600d87de2c894117d5a0b1eb753dbea6a57c756
FedorMerenkov <worend324@gmail.com>
2026-04-02 10:18:05 +0400

    complete day 8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,16 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
-
+//        int count = 1;
+//        StringBuilder count1 = new StringBuilder(count);
+//        for (int i = 1; i <= 20000; i++) {
+//            count1.append(i).append(" ");
+//        }
+//        System.out.println(s);
+        String count2 = " ";
+        for (int i = 1; i <= 20000; i++) {
+            count2 += i + " ";
+        }
+        System.out.println(count2);
     }
 }
