d907389ea289ace075fcb7ad49f2a4b14ea4569c
FedorMerenkov <worend324@gmail.com>
2026-04-10 00:46:58 +0400

    exit extends, abstract class day 9 task 1-2

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -1,7 +1,15 @@
 package day9.Task1;
 
+import java.util.Arrays;
+
 public class Task1 {
     public static void main(String[] args) {
+        Student student = new Student("Ваня", "103");
+        Teacher teacher = new Teacher("Эдуарт", "Матан");
+        System.out.println(student.getGroupName());
+        System.out.println(teacher.getSubjectName());
+        student.printInfo();
+        teacher.printInfo();
 
     }
 }
--- src/main/java/day9/Task2/TestFigures.java
+++ src/main/java/day9/Task2/TestFigures.java
@@ -1,7 +1,45 @@
 package day9.Task2;
 
+import java.math.MathContext;
+import java.util.Arrays;
+
 public class TestFigures {
     public static void main(String[] args) {
+        Figure[] figures = {
+                new Triangle(10, 10, 10, "Red"),
+                new Triangle(10, 20, 30, "Green"),
+                new Triangle(10, 20, 15, "Red"),
+                new Rectangle(5, 10, "Red"),
+                new Rectangle(40, 15, "Orange"),
+                new Circle(4, "Red"),
+                new Circle(10, "Red"),
+                new Circle(5, "Blue")
+        };
+        double perimeter = calculateRedPerimeter(figures);
+        double area = calculateRedArea(figures);
+        System.out.println(perimeter);
+        System.out.println(area);
+    }
+
+    public static double calculateRedPerimeter(Figure[] figures) {
+        double summP = 0;
+        for (Figure figure : figures) {
+            if (figure.color.equals("Red")) {
+                summP = summP + figure.perimeter();
+            }
+        }
+        return summP;
 
     }
+
+    public static double calculateRedArea(Figure[] figures) {
+        double summS = 0;
+        for (Figure figure : figures) {
+            if (figure.color.equals("Red")) {
+                summS = summS + figure.area();
+            }
+        }
+        return summS;
+    }
+
 }
