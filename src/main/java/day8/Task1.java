f84e3c083faac780e570de088dd3caefc487cf81
FedorMerenkov <worend324@gmail.com>
2026-04-22 19:59:06 +0400

    Day 8 (#8)
    * complete day 8
    
    * generate toString day 8 task 2
    
    * fix name and comments

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,16 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        int count = 1;
+        StringBuilder countStringBuilder = new StringBuilder(count);
+        for (int i = 1; i <= 20000; i++) {
+            countStringBuilder.append(i).append(" ");
+        }
+        System.out.println(countStringBuilder);
+        String countString = " ";
+        for (int i = 1; i <= 20000; i++) {
+            countString += i + " ";
+        }
+        System.out.println(countString);
     }
 }
