f93fa1b36de20d8a2620f28f0a4fa1c7261b9780
FedorMerenkov <worend324@gmail.com>
2026-05-04 16:11:48 +0400

    complete day 13

---
+++ src/main/java/day13/User.java
@@ -0,0 +1,47 @@
+package day13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String username;
+    private List<User> subscription;
+
+    public User(String username) {
+        this.username = username;
+        this.subscription = new ArrayList<>();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public List<User> getSubscriptions() {
+        return subscription;
+    }
+
+    public void subscribe(User user) {
+        this.subscription.add(user);
+    }
+
+    public boolean isSubscribed(User user) {
+        for (User currentUser : subscription) {
+            if (currentUser.getUsername().equals(user.getUsername())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isFriend(User user) {
+        return this.isSubscribed(user) && user.isSubscribed(this);
+    }
+
+    public void sendMessage(User user, String text) {
+        MessageDatabase.addNewMessage(this, user, text);
+    }
+
+    public String toString() {
+        return username;
+    }
+}
