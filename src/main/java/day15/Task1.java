09c8015adba7c4a18207c1ef98d2eb1eda8788a2
FedorMerenkov <worend324@gmail.com>
2026-06-30 23:32:09 +0400

    complete day 15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,25 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:\\Users\\user\\Desktop\\Java_training\\JavaMarathon2020\\src\\main\\resources\\shoes.csv");
+        Scanner scanner = new Scanner(file);
+
+        File fileOut = new File("C:\\Users\\user\\Desktop\\Java_training\\JavaMarathon2020\\src\\main\\resources\\missing_shoes.txt");
+        PrintWriter pw = new PrintWriter(fileOut);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] array = line.split(";");
+            if (Integer.parseInt(array[2]) == 0) {
+                pw.println(line);
+            }
+        }
+        pw.close();
+        scanner.close();
     }
 }
