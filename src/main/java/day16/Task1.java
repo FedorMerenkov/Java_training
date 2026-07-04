ea6bb2dff82bbe0e9a45a9ed87fa7dd3f89f6ecc
FedorMerenkov <worend324@gmail.com>
2026-07-04 21:30:33 +0400

    complete day 16 (#15)

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,7 +1,32 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("C:\\Users\\user\\Desktop\\Java_training\\JavaMarathon2020\\src\\main\\java\\day16\\input");
+        printResult(file);
+    }
+
+    public static void printResult(File file1e) {
+        try {
+
+            Scanner scanner = new Scanner(file1e);
+
+            String line = scanner.nextLine();
+            String[] number = line.split(" ");
+
+            int sum = 0;
+            for (String numbers : number) {
+                sum += Integer.parseInt(numbers);
+            }
+            double result = sum / (double) number.length;
+            System.out.printf(result + " --> %.3f", result);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
 
     }
 }
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,54 @@
 package day16;
 
-public class Task2 {
-    public static void main(String[] args) {
+import java.io.FileNotFoundException;
+import java.util.Random;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
+public class Task2 {
+    public static void main(String[] args) throws IOException {
+        File file1 = new File("C:\\Users\\user\\Desktop\\Java_training\\JavaMarathon2020\\file1.txt");
+        file1.createNewFile();
+        PrintWriter pw1 = new PrintWriter(file1);
+        for (int i = 0; i < 1000; i++) {
+            Random random = new Random();
+
+            int x = random.nextInt(100);
+            pw1.print(x + " ");
+        }
+        pw1.close();
+        File file2 = new File("C:\\Users\\user\\Desktop\\Java_training\\JavaMarathon2020\\file2.txt");
+        file2.createNewFile();
+        PrintWriter pw2 = new PrintWriter(file2);
+        Scanner scanner1 = new Scanner(file1);
+
+        String line = scanner1.nextLine();
+        String[] number = line.split(" ");
+        double sum = 0;
+        int i = 0;
+        while (i < 1000) {
+            for (int j = 0; j < 20; j++) {
+                sum += Integer.parseInt(number[j + i]);
+            }
+            i += 20;
+
+            double result = sum / 20;
+            pw2.println(result);
+            sum = 0;
+        }
+
+        pw2.close();
+        System.out.println((int) printResult(file2));
+    }
+
+    public static double printResult(File file) throws FileNotFoundException {
+        Scanner scanner2 = new Scanner(file);
+        double result = 0;
+        while (scanner2.hasNextLine()) {
+            result += Double.parseDouble(scanner2.nextLine());
+        }
+        return result;
     }
 }
